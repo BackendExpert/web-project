02c63266542c15bb2526444e7bdb77d74759832d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-02 19:52:11 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/footer/FooterM.tsx
+++ src/footer/FooterM.tsx
@@ -3,7 +3,19 @@ const FooterM = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-6 sm:px-8 px-5 py-4">
        <div>
-            <h1 className="">About</h1>
+            <h1 className="mb-1 font-semibold text-2xl">About</h1>
+            <ul>
+                <li><a href=""></a></li>
+            </ul>
+       </div>
+       <div>
+            <h1 className="mb-1 font-semibold text-2xl">Research</h1>
+       </div>
+       <div>
+            <h1 className="mb-1 font-semibold text-2xl">Dissemination</h1>
+       </div>
+       <div>
+            <h1 className="mb-1 font-semibold text-2xl">Contact</h1>
        </div>
     </div>
   )
