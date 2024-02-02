a24692d21ae4fc5fcdc20cd8a868ad16f906912a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-02 20:37:35 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import Footer from "./footer/Footer";
+import Tb from "./TestBody/Tb";
 
 export default function App() {
   return (
+    <div>
+        <Tb />    
+        <Footer />
+    </div>
 
-    <Footer />
   )
 }
