a5b953227af94326ab0dc8416055efa5cf206857
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-02 18:43:06 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
+import Footer from "./footer/Footer";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Footer />
   )
 }
