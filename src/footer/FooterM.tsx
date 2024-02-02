652340aaff55bb66abc0cf5041a3cef0a2f7c0b3
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-02 19:45:23 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/footer/FooterM.tsx
+++ src/footer/FooterM.tsx
@@ -2,7 +2,9 @@
 const FooterM = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-6 sm:px-8 px-5 py-4">
-       
+       <div>
+            <h1 className="">About</h1>
+       </div>
     </div>
   )
 }
