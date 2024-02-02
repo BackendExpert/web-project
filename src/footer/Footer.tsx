a480ece53763f12d305136f45af1d97182f88b5f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-02 18:46:13 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -1,7 +1,41 @@
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+import LogoSl from '../assets/pngegg.png'
+import fb from '../assets/faceb.png'
+import inster from '../assets/inster.png'
+import linked from '../assets/linkedin.png'
+import tx from '../assets/tx.png'
+import yt from '../assets/yt.png'
 
-export default Footer
+const Footer = () => {
+    return (      
+
+        <footer className="bg-sky-900 text-white">
+            <div className="container mx-auto h-auto px-12 py-8">
+                <div className="mx-auto w-full max-w-screen-xl">
+                {/* <div className="sm:flex sm:items-center sm:justify-between">    */}
+                    <div className="px-4 py-6 dark:bg-gray-700 md:flex md:items-center md:justify-between">
+                        <div className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                            <img src={LogoSl} className="h-16" alt="Flowbite Logo" />
+                            <div className="pl-2 text-xs">
+                                <ul>
+                                    <li>National Institute of Fundamental Studies.</li>
+                                    <li>Ministry of Education</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <ul className="flex flex-wrap justify-end sm:mb-0">
+                            <li className='pl-4'><img src={fb} alt="Facebook" /></li>
+                            <li className='pl-4'><img src={tx} alt="Tuiter" /></li>
+                            <li className='pl-4'><img src={inster} alt="Inster" /></li>
+                            <li className='pl-4'><img src={yt} alt="Youtube" /></li>    
+                            <li className='pl-4'><img src={linked} alt="linked" /></li>                         
+                        </ul>
+                    </div>
+                {/* </div> */}
+                <p className="text-center text-xs">&copy; National Institute of Fundamental Studies. Hanthana Road, Kandy, Sri Lanka</p>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
