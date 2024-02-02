49aa90dddb0c3a55524efeb098a7a15d51964eb8
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-02 19:11:07 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -11,10 +11,10 @@ const Footer = () => {
         <footer className="bg-sky-900 text-white">
             <div className="container mx-auto h-auto px-8 py-8">
                 <div className="mx-auto w-full max-w-screen-xl">
-                {/* <div className="sm:flex sm:items-center sm:justify-between">    */}
+                    dsadas
                     <div className="px-4 py-6 dark:bg-gray-700 md:flex md:items-center md:justify-between">
                         <div className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                            <img src={LogoSl} className="h-16" alt="Flowbite Logo" />
+                            <img src={LogoSl} className="h-16" alt="SlLogo" />
                             <div className="pl-2 text-xs">
                                 <ul>
                                     <li>National Institute of Fundamental Studies.</li>
@@ -30,7 +30,6 @@ const Footer = () => {
                             <li className='pl-4'><img src={linked} alt="linked" /></li>                         
                         </ul>
                     </div>
-                {/* </div> */}
                 <p className="text-center text-xs">&copy; National Institute of Fundamental Studies. Hanthana Road, Kandy, Sri Lanka</p>
                 </div>
             </div>
