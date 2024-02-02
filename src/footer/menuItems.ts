b8d7addd675ae3b2595202c3558a4e754f2e2a07
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-02 19:32:22 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/footer/menuItems.ts
+++ src/footer/menuItems.ts
@@ -4,5 +4,12 @@ export const About = [
     { name: "Research Council" , link: "#"},
     { name: "Alumni" , link: "#"},
     { name: "Join" , link: "#"},
-]
+];
+
+export const Research = [
+    {name: "Research Divisions", link: "#"},
+    {name: "Research Collaboration", link: "#"},
+    {name: "Research Funding", link: "#"},
+    {name: "Young Scientist Association (YSA)", link: "#"},    
+];
 
