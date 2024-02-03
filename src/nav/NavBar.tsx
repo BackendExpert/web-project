d02f273086c39680a134dffb8d3a8de6232b1d6a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-03 07:50:12 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/nav/NavBar.tsx
+++ src/nav/NavBar.tsx
@@ -1,7 +1,21 @@
+import LogoSl from '../assets/pngegg.png'
+
 const NavBar = () => {
   return (
     <div>
-      
+        <nav>
+        <div className="px-4 py-6 dark:bg-gray-700 md:flex md:items-center md:justify-between">
+          <div className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+            <img src={LogoSl} className="h-16" alt="SlLogo" />
+                <div className="pl-2 text-xs">
+                    <ul>
+                      <li>National Institute of Fundamental Studies.</li>
+                      <li>Ministry of Education</li>
+                    </ul>
+                  </div>
+                </div>
+             </div>
+        </nav>
     </div>
   )
 }
