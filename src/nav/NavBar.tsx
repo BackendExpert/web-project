2048d1d1bb0fdec98dd6929c04fd3d8e6f1b811f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-03 08:52:27 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/nav/NavBar.tsx
+++ src/nav/NavBar.tsx
@@ -4,17 +4,19 @@ import NavItems from './NavItems'
 const NavBar = () => {
   return (
     <div className='bg-gray-600 bg-opacity-20'>
-        <nav className='container mx-auto h-auto px-2 py-2'>
-        <div className="px-2 py-2 dark:bg-gray-700 md:flex md:items-center md:justify-between">
-          <div className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-            <img src={LogoSl} className="h-8" alt="SlLogo" />
-                <div className="pl-2 text-xs">
-                    <ul>
-                      <li className="text-blue-800 text-4xl">NIFS</li>
-                    </ul>
-                </div>
-            </div>
-            <NavItems />
+        <nav className='container mx-auto h-auto px-8 py-2'>
+        <div className="mx-auto w-full max-w-screen-xxl">
+          <div className="px-2 py-2 dark:bg-gray-700 md:flex md:items-center md:justify-between">
+            <div className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+              <img src={LogoSl} className="h-8" alt="SlLogo" />
+                  <div className="pl-2 text-xs">
+                      <ul>
+                        <li className="text-blue-800 text-4xl">NIFS</li>
+                      </ul>
+                  </div>
+              </div>
+              <NavItems />
+          </div>
           </div>
         </nav>
     </div>
