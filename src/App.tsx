1eea0203dcffed812af533419327c86800530d0b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-03 08:32:53 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import Footer from "./footer/Footer";
-import NavBar from "./nav/NavBar";
 import Tb from "./TestBody/Tb";
 
 export default function App() {
   return (
     <div>
-        <NavBar />
         <Tb />    
         <Footer />
     </div>
--- src/nav/NavBar.tsx
+++ src/nav/NavBar.tsx
@@ -2,7 +2,7 @@ import LogoSl from '../assets/pngegg.png'
 
 const NavBar = () => {
   return (
-    <div>
+    <div className='bg-red-800'>
         <nav className='container mx-auto h-auto px-2 py-2'>
         <div className="px-2 py-2 dark:bg-gray-700 md:flex md:items-center md:justify-between">
           <div className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
