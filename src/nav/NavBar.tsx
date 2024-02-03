c87eff871a42514d08b087506063721b687598d5
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-03 09:19:13 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/nav/NavBar.tsx
+++ src/nav/NavBar.tsx
@@ -4,7 +4,7 @@ import NavItems from './NavItems'
 const NavBar = () => {
   return (
     <div className='bg-gray-600 bg-opacity-20'>
-        <nav className='container mx-auto h-auto px-8 py-2'>
+        <nav className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
         <div className="mx-auto w-full max-w-screen-xxl">
           <div className="px-2 py-2 dark:bg-gray-700 md:flex md:items-center md:justify-between">
             <div className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
