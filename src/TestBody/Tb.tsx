3adb1b43bedd5008840abaec5d95d98752f39669
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-03 08:10:48 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/TestBody/Tb.tsx
+++ src/TestBody/Tb.tsx
@@ -2,28 +2,8 @@
 const Tb = () => {
   return (
     <div className="bg-white h-auto">
-        <div className="container mx-auto py-12 px-8">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga excepturi nulla sint consectetur, itaque, tenetur vitae soluta atque nisi vero recusandae numquam aliquam? Nam pariatur illo atque aspernatur ipsa eligendi!
-            Deserunt tempore ipsum inventore illum sed odio nihil quis eum possimus veniam impedit enim sit repudiandae similique blanditiis, sequi reiciendis laudantium exercitationem voluptate nam! Non dolorum illo veritatis sapiente recusandae.
-            Reprehenderit nostrum vero dolores optio molestiae eos recusandae ut, excepturi autem qui minima, illo quisquam? Aperiam, dolorum obcaecati quaerat ullam, distinctio deleniti perspiciatis modi, cupiditate dolore repudiandae voluptatem reprehenderit exercitationem!
-            Odio quibusdam beatae ea deleniti, aliquid a eaque quam saepe sit, expedita eius laboriosam consequatur officia magnam praesentium quidem ratione culpa quia? Inventore, cupiditate? Voluptate, in. Itaque tenetur voluptates ab.
-            Beatae molestias et nemo, hic quidem quae velit ducimus amet minus maxime! Necessitatibus itaque asperiores veritatis ducimus modi libero laborum ipsum ab molestias? Veniam inventore obcaecati labore, temporibus officia nisi.
-            Nostrum a ipsa deserunt temporibus quam vel accusantium, voluptate repudiandae sapiente adipisci itaque dolor similique nam perspiciatis vitae quibusdam tempora! Cupiditate, earum ipsa laborum suscipit ratione ducimus repudiandae quam architecto.
-            Reiciendis repellendus quo ipsam illo asperiores. Sequi harum dicta reiciendis necessitatibus ab beatae cum quis vero. Odit nam, rerum aliquid cumque quis reiciendis dignissimos neque error blanditiis, voluptas suscipit nihil.
-            Tenetur aut repellendus illo officia impedit atque, minus ipsam facilis ratione odit eligendi sequi odio, eum cupiditate soluta consectetur repellat nobis? Libero, explicabo delectus consequuntur impedit tempora quaerat atque enim?
-            Expedita, debitis deleniti tenetur consequuntur dicta, earum neque possimus facere magni ullam quasi consectetur quos quisquam soluta tempore modi obcaecati nisi sequi? Magni accusantium fuga ea dolore sint? Modi, ex!
-            Libero quibusdam est iusto ratione laboriosam saepe et ipsam! Corrupti nesciunt ipsum pariatur eveniet. Maxime eveniet nisi dolorum nemo, ipsum accusantium ipsa harum magni, facere corrupti repudiandae reprehenderit eos et!
-            Recusandae, in. Ab asperiores pariatur earum reiciendis doloribus numquam enim aliquid esse, exercitationem sint totam sequi iusto magnam, ad corrupti alias. Fugiat deleniti minima at ipsam, sapiente dolores expedita amet!
-            Harum, similique perspiciatis. Officia mollitia, architecto pariatur blanditiis omnis magnam consequuntur minima id. Quas aut cum quod quia, pariatur mollitia deserunt sunt dolorum sint nulla incidunt voluptatum molestiae fugit odit.
-            Dolorem officiis maxime est quas nostrum dolores maiores soluta quis repellat beatae minus inventore quidem autem commodi, dicta itaque veniam labore! Hic eligendi assumenda accusantium optio quisquam omnis alias nemo?
-            Expedita itaque quidem quisquam magnam placeat nemo voluptas dolores accusantium obcaecati laboriosam consequatur impedit esse provident mollitia eligendi temporibus quia enim dignissimos magni, dicta aut tenetur! Officiis eius nulla non.
-            Impedit accusamus dolore nisi. Amet voluptatibus dolores mollitia natus id harum illo ad maxime ipsam, ullam temporibus exercitationem hic beatae rem recusandae ipsa quae quis iure cumque quas perferendis. Nostrum!
-            Est odio ex placeat unde expedita quaerat. Dignissimos vel, numquam nobis ut animi voluptate illum? Assumenda possimus iure repellat unde tempore ipsam excepturi veritatis delectus ipsum fugiat quo, dolorum hic?
-            Qui, molestiae! Illo quaerat velit nihil mollitia, inventore voluptatibus magni ipsum eaque eligendi perspiciatis voluptatum modi blanditiis laudantium maiores omnis expedita quis incidunt voluptates sint veniam. Provident possimus non eligendi.
-            Recusandae omnis quisquam totam! Perspiciatis iste repellendus in omnis. Necessitatibus maiores, facilis impedit sed id optio harum nemo, error dicta placeat, quam recusandae. Distinctio aliquam rerum praesentium temporibus atque voluptate?
-            Officiis placeat, quas quis, itaque vel a voluptatum maxime fuga, cum aspernatur voluptate! Iure voluptatem rem sed, perferendis enim voluptates odio neque repudiandae eius inventore ab sit reprehenderit tenetur saepe.
-            Recusandae voluptates sunt numquam aliquid aspernatur iusto odit veniam dolores consequuntur ipsum optio, libero quam? Quisquam ex eveniet quod voluptates suscipit, fugiat animi dolores nemo libero culpa voluptatem dolorem. Quos?
-        </div>
+        style={{'': `url(${fetchedUrl})`}}
+        <div className="" ></div>
     </div>
 
   )
