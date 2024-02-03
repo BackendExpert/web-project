b2ea9222aae0d4e00c0ebf274e5432bae2d9f963
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-03 08:56:50 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/nav/NavItems.tsx
+++ src/nav/NavItems.tsx
@@ -2,13 +2,13 @@ const NavItems = () => {
   return (
     <div className="text-white fs-semibold">
         <ul className="flex flex-wrap justify-end sm:mb-0">
-            <li><a href="">Home</a></li>     
-            <li><a href="">About</a></li>     
-            <li><a href="">News & Events</a></li>                      
-            <li><a href="">Academic</a></li>     
-            <li><a href="">Publication</a></li>     
-            <li><a href="">Careers</a></li>     
-            <li><a href="">Contact</a></li>     
+            <li className="px-4"><a href="">Home</a></li>     
+            <li className="px-4"><a href="">About</a></li>     
+            <li className="px-4"><a href="">News & Events</a></li>                      
+            <li className="px-4"><a href="">Academic</a></li>     
+            <li className="px-4"><a href="">Publication</a></li>     
+            <li className="px-4"><a href="">Careers</a></li>     
+            <li className="px-4"><a href="">Contact</a></li>     
         </ul>
     </div>
   )
