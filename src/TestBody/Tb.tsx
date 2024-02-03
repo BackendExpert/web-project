4b5fa771b05e26ce9821ba1a3db5aca0af697431
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-03 08:20:39 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/TestBody/Tb.tsx
+++ src/TestBody/Tb.tsx
@@ -1,9 +1,10 @@
 
 const Tb = () => {
   return (
-    <div className="bg-white h-auto">
-        style={{'': `url(${fetchedUrl})`}}
-        <div className="" ></div>
+    <div className="bg-white h-auto">        
+        <div className="bg-[url('https://c4.wallpaperflare.com/wallpaper/825/184/17/car-vehicle-super-car-supercars-wallpaper-preview.jpg')] w-full h-[250px] bg-cover bg-center bg-no-repeat mb-8 md:h-[550px]" >
+            Hi all
+        </div>
     </div>
 
   )
