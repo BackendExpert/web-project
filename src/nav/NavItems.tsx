4b03def4408ba0475f718e69803a0076a79c1719
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-03 08:42:03 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/nav/NavItems.tsx
+++ src/nav/NavItems.tsx
@@ -1,6 +1,10 @@
 const NavItems = () => {
   return (
-    <div>NavItems</div>
+    <div className="text-white fs-semibold">
+        <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-6 sm:px-8 px-1 py-1">
+            hi all
+        </div>
+    </div>
   )
 }
 
