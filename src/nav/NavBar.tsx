830097b282d1666693f619157eeafe1aeaa276b2
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-04 22:20:05 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/nav/NavBar.tsx
+++ src/nav/NavBar.tsx
@@ -19,12 +19,12 @@ const NavBar = () => {
           <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-16':'top-[-490px]'}`}>
             
             <li className="md:ml-8 text xl md:my-0 my-7">
-            <span className="">Helloworld</span>
-              <ul>
-                <li><a href="">Hello</a></li>
-                <li><a href="">Hello</a></li>
-                <li><a href="">Hello</a></li>
-                <li><a href="">Hello</a></li>
+              <span className="">Helloworld <span className=""><IonIcon name="chevron-down-outline"></IonIcon></span></span>
+              <ul className={`md:items-center md:pb-0 pb-1`}>
+                <li><a href="#">Hello</a></li>
+                <li><a href="#">Hello</a></li>
+                <li><a href="#">Hello</a></li>
+                <li><a href="#">Hello</a></li>
               </ul>
             </li>
             <li className="md:ml-8 text xl md:my-0 my-7">
