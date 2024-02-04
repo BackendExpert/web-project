020881d25facc0e51961b2f30c2989490e3c5b90
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-04 19:16:58 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/nav/NavBar.tsx
+++ src/nav/NavBar.tsx
@@ -1,7 +1,9 @@
 const NavBar = () => {
   return (
-    <div className="">
-
+    <div className="shadow-md w-full fixed top-0 left-0">
+      <div className="md:flex bg-white py-4">
+        
+      </div>
     </div>
   )
 }
