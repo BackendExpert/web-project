b3c8f17e05a340c02498ab8065b3a89419b170c7
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-04 18:27:16 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/nav/NavBar.tsx
+++ src/nav/NavBar.tsx
@@ -1,28 +1,6 @@
-import React, { useState } from 'react'
-
-
 const NavBar = () => {
   return (
-    <div className='shadow-md w-full fixed top-0 left-0'>
-      <div className='md:flex items-center justify-between bg-white py-4 md:px-10 px-7'>
-      <div className='font-bold text-2xl cursor-pointer flex items-center font-[Poppins] 
-      text-gray-800'>
-        <span className='text-3xl text-indigo-600 mr-1 pt-2'>
-        </span>
-        Designer
-      </div>
-      
-      <div onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden'>
-        <span name="name={open ? 'close':'menu'}"></span>
-      </div>
-
-      <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ':'top-[-490px]'}`}>
-
-      </ul>
-      </div>
-    </div>
-
-
+    <div></div>
   )
 }
 
