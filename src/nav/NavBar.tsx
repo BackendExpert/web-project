27b31837359162bc2d397ed0981f735b5d6fb470
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-04 22:36:40 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/nav/NavBar.tsx
+++ src/nav/NavBar.tsx
@@ -4,29 +4,20 @@ import IonIcon from "@reacticons/ionicons"
 
 const NavBar = () => {
   let [open, OpenMenu] = useState(false);
-  let [subm1, OpenSubM1] = useState(false);
+
   return (
     <div className="shadow-md w-full fixed top-0 left-0 bg-blue-900">
       <div className="md:flex items-center justify-between bg-white py-4 md:px-20 px-7">
         <div className="font-bold text-2xl cursor-pointer flex items-center text-white">
-          <span className="text-3x1 text-indigo-600">
+          <span className="text-3x1 text-indigo-600 md:flex">
             <img src={SlLogo} alt="" className="h-8 w-8"/>
+            <span className="px-4">NIFS</span>
           </span>
           </div>
           <div onClick={() => OpenMenu(!open)} className="text-xl absolute right-8 top-5 cursor-pointer md:hidden">
             <span><IonIcon name={open ? "close" : "menu"} /></span>
           </div>
           <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-16':'top-[-490px]'}`}>
-            
-            <li className="md:ml-8 text xl md:my-0 my-7">
-              <span className="">Helloworld <span className=""><IonIcon name="chevron-down-outline"></IonIcon></span></span>
-              <ul className={`md:items-center md:pb-0 pb-1`}>
-                <li><a href="#">Hello</a></li>
-                <li><a href="#">Hello</a></li>
-                <li><a href="#">Hello</a></li>
-                <li><a href="#">Hello</a></li>
-              </ul>
-            </li>
             <li className="md:ml-8 text xl md:my-0 my-7">
               <a href="#" className="text-gray-800 hover:text-gray-400 duration-500">Home</a>
             </li>
