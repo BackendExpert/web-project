fafcaf9b04f9c27039d1936f3fbf84254f77a369
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-04 19:26:36 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/TestBody/Tb.tsx
+++ src/TestBody/Tb.tsx
@@ -6,6 +6,18 @@ const Tb = () => {
           
         <div className="bg-[linear-gradient(to_right_bottom,rgba(190,190,190,0.8),rgba(0,0,0,0.8)),url('https://c4.wallpaperflare.com/wallpaper/825/184/17/car-vehicle-super-car-supercars-wallpaper-preview.jpg')] w-full h-[250px] bg-cover bg-center bg-no-repeat mb-8 md:h-[550px] bg-opacity-75" >
         <NavBar />    
+          <div className="">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem necessitatibus eos animi quasi deserunt vitae odit dolorum. Amet doloremque non ipsa quibusdam tempore enim quisquam repudiandae eligendi velit, libero hic?
+            Voluptates eos dolorem, aliquam molestiae quos autem exercitationem eius pariatur sed esse nulla perferendis qui quas deserunt blanditiis cum laborum consequuntur ducimus facilis impedit perspiciatis quidem mollitia rerum itaque? Nostrum.
+            Voluptatem minima nesciunt quo itaque! Amet, qui veritatis alias nihil voluptas doloremque ut unde laudantium iste tempora omnis, deleniti nulla quas perferendis architecto, odit incidunt quae. Repellat, itaque? Natus, consequatur?
+            Numquam beatae deleniti provident maxime. Eum officia expedita, natus hic voluptates saepe vero suscipit aperiam fugiat illo enim explicabo magnam asperiores consequatur perferendis. Velit numquam mollitia, doloribus autem temporibus minima?
+            Nam neque maiores sequi saepe, fugiat error accusamus non eaque vitae! Veritatis, saepe quae hic sed dolores officia corrupti, nisi ipsam incidunt ipsum dolorum, quam dolore esse blanditiis sapiente architecto.
+            Eaque officia sapiente nihil perferendis iste id recusandae voluptatibus eius dolore, consequuntur similique ullam tempora delectus voluptas eligendi maxime dolores. Deserunt nihil cupiditate neque quia repudiandae eos dolores, quas sapiente.
+            Placeat adipisci, sed optio, nemo impedit maxime asperiores perferendis aliquid cum soluta praesentium, quia porro cupiditate pariatur corrupti? Iste maiores fugiat quisquam assumenda reprehenderit culpa voluptates eligendi placeat natus illo.
+            Vero, totam error nemo et deleniti similique nesciunt enim hic tempora quod vel consequuntur iure dolor. Reprehenderit, laboriosam amet. Quae, soluta laboriosam? Asperiores exercitationem ullam tempore! Repellendus eius assumenda quasi!
+            Veritatis eveniet quasi rem eaque officiis nulla a, consectetur sit voluptatem soluta quam illo neque voluptas asperiores quo ullam cumque necessitatibus nisi aut. Culpa, voluptate vel. In aspernatur excepturi blanditiis.
+            Consequatur exercitationem nostrum facilis tenetur minus eius, quaerat vel. Magnam repudiandae veritatis quidem, illo nulla quisquam incidunt repellendus eaque, reiciendis possimus ex vel nam dignissimos eveniet, dolorem illum doloribus eos.
+          </div>
         </div>
     </div>
 
