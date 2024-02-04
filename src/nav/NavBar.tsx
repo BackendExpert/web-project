ec2b253c86ebef2a31383a89146b3065157312f6
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-04 19:29:28 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/nav/NavBar.tsx
+++ src/nav/NavBar.tsx
@@ -3,8 +3,8 @@ import SlLogo from "../assets/pngegg.png"
 
 const NavBar = () => {
   return (
-    <div className="shadow-md w-full fixed top-0 left-0">
-      <div className="container mx-auto h-auto px-1 py-4">
+    <div className="shadow-md w-full fixed top-0 left-0 bg-blue-900">
+      <div className="container mx-auto h-auto px-10 py-4">
         <div className="font-bold text-2xl cursor-pointer flex items-center font-[Poppins] text-gray-800">
           <span className="text-3x1 text-indigo-600">
             <img src={SlLogo} alt="" className="h-8 w-8"/>
