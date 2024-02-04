a986331ec26128ed847fd274d07b6515bb416cdf
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-04 22:13:16 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/nav/NavBar.tsx
+++ src/nav/NavBar.tsx
@@ -17,8 +17,15 @@ const NavBar = () => {
             <span><IonIcon name={open ? "close" : "menu"} /></span>
           </div>
           <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-16':'top-[-490px]'}`}>
+            
             <li className="md:ml-8 text xl md:my-0 my-7">
-              <a href="#" className="text-gray-800 hover:text-gray-400 duration-500">Home</a>
+            <span className="">Helloworld</span>
+              <ul>
+                <li><a href="">Hello</a></li>
+                <li><a href="">Hello</a></li>
+                <li><a href="">Hello</a></li>
+                <li><a href="">Hello</a></li>
+              </ul>
             </li>
             <li className="md:ml-8 text xl md:my-0 my-7">
               <a href="#" className="text-gray-800 hover:text-gray-400 duration-500">Home</a>
