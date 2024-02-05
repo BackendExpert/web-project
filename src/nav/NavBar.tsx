13723fbd4fa2ed92af5aa9ad1175ee19e064e451
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-06 05:03:12 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/nav/NavBar.tsx
+++ src/nav/NavBar.tsx
@@ -2,9 +2,11 @@ import SLlogo from '../assets/pngegg.png'
 
 const NavBar = () => {
   return(
-    <div className='py-4 px-4 md:flex'>
-      <img className='h-16' src={SLlogo} />
-      <span className="px-2 py-4 text-2xl">NIFS</span>
+    <div>
+      <div className='py-4 px-4 md:flex'>
+        <img className='h-16' src={SLlogo} />
+        <span className="px-2 py-4 text-2xl">NIFS</span>
+      </div>
     </div>
   )
 }
