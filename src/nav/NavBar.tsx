c72524989cec8395a088514c5a8cd2361bbebc22
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-05 05:37:20 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/nav/NavBar.tsx
+++ src/nav/NavBar.tsx
@@ -5,6 +5,7 @@ import IonIcon from "@reacticons/ionicons"
 const NavBar = () => {
   let [open, OpenMenu] = useState(false);
   let [opensbm, OpensMenu] = useState(false);
+  let [newSubm, NewSubOpen] = useState(false);
   return (
     <div className="shadow-md w-full fixed top-0 left-0 bg-blue-900">
       <div className="md:flex items-center justify-between bg-white py-4 md:px-20 px-7">
@@ -19,12 +20,22 @@ const NavBar = () => {
           </div>
           <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-16':'top-[-490px]'}`}>
             <li className="md:ml-8 text xl md:my-0 my-7">
-              <span onClick={() => OpensMenu(!opensbm)} className="cursor-pointer">Hellooo World <span><IonIcon name={opensbm ? "chevron-down-outline" : "chevron-up-outline"} /></span></span>
-              <ul className={`items-center pb-0 pb-12 absolute static bg-white z-auto z-[-1] left-0 w-full w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${opensbm ? 'top-16':'top-[-490px]'}`}>
-                  <li><a href="#">hellow W</a></li>
-                  <li><a href="#">hellow W</a></li>
-                  <li><a href="#">hellow W</a></li>
-                  <li><a href="#">hellow W</a></li>
+              <span onClick={() => OpensMenu(!opensbm)} className="cursor-pointer">Hellooo World <span><IonIcon name={opensbm ? "chevron-up-outline" : "chevron-down-outline"} /></span></span>
+              <ul className={`items-center pb-0 pb-12 absolute static bg-white z-auto z-[-1] left-100 w-full w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${opensbm ? 'top-16':'top-[-490px]'}`}>
+                  <li className="md:py-5 px-4"><a href="#">hellow W</a></li>
+                  <li className="md:py-5 px-4"><a href="#">hellow W</a></li>
+                  <li className="md:py-5 px-4">
+                  <span onClick={() => NewSubOpen(!newSubm)} className="cursor-pointer">Hellooo World <span><IonIcon name={newSubm ? "chevron-up-outline" : "chevron-down-outline"} /></span></span>
+                    <ul className={`items-center pb-0 pb-12 absolute static bg-white z-auto z-[-1] left-100 w-full w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${newSubm ? 'top-16':'top-[-490px]'}`}>
+                      <li className=""><a href="#">new Sub 1</a></li>
+                      <li className=""><a href="#">new Sub 1</a></li>
+                      <li className=""><a href="#">new Sub 1</a></li>
+                    </ul>
+                  </li>
+                  <li className="md:py-5 px-4"><a href="#">hellow W</a></li>
+                  <li className="md:py-5 px-4"><a href="#">hellow W</a></li>
+                  <li className="md:py-5 px-4"><a href="#">hellow W</a></li>
+                  <li className="md:py-5 px-4"><a href="#">hellow W</a></li>
               </ul>
             </li>
             <li className="md:ml-8 text xl md:my-0 my-7">
