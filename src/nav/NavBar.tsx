1c5dca1c7a7fca78fd5c8974919328109972d057
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-06 09:22:30 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/nav/NavBar.tsx
+++ src/nav/NavBar.tsx
@@ -43,17 +43,17 @@ const NavBar = () => {
                   <div>
                   <span className="text-2xl">Leadership</span>
                       <ul className="px-4 py-2">
-                        <li className="pb-[3px]"><a href="" className="">Board of Govrnors</a></li>
-                        <li className="pb-[3px]"><a href="" className="">Chairman</a></li>
-                        <li className="pb-[3px]"><a href="" className="">Director</a></li>
-                        <li className="pb-[3px]"><a href="" className="">Research Council</a></li>
+                        <li className="pb-[3px] duration-500 hover:bg-white hover:px-2 hover:text-gray-700 rounded-sm font-medium"><a href="" className="">Board of Govrnors</a></li>
+                        <li className="pb-[3px] duration-500 hover:bg-white hover:px-2 hover:text-gray-700 rounded-sm font-medium"><a href="" className="">Chairman</a></li>
+                        <li className="pb-[3px] duration-500 hover:bg-white hover:px-2 hover:text-gray-700 rounded-sm font-medium"><a href="" className="">Director</a></li>
+                        <li className="pb-[3px] duration-500 hover:bg-white hover:px-2 hover:text-gray-700 rounded-sm font-medium"><a href="" className="">Research Council</a></li>
                       </ul>
                   </div>
                   <div>
                   <span className="text-2xl">Society</span>
                       <ul className="px-4 py-2">
-                        <li className="pb-[3px]"><a href="" className="">Young Scientist Association</a></li>
-                        <li className="pb-[3px]"><a href="" className="">Welfare</a></li>
+                        <li className="pb-[3px] duration-500 hover:bg-white hover:px-2 hover:text-gray-700 rounded-sm font-medium"><a href="" className="">Young Scientist Association</a></li>
+                        <li className="pb-[3px] duration-500 hover:bg-white hover:px-2 hover:text-gray-700 rounded-sm font-medium"><a href="" className="">Welfare</a></li>
                       </ul>
                   </div>
                   <div>
