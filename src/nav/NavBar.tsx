2279dcbd40c42c543fdfa767e10c21f0661cc424
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-06 06:03:38 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/nav/NavBar.tsx
+++ src/nav/NavBar.tsx
@@ -16,9 +16,9 @@ const NavBar = () => {
             <div onClick={() => OpenMenu(!open)} className="text-xl absolute right-8 top-5 cursor-pointer">
                 <span><IonIcon name={open ? "close" : "menu"} /></span>
             </div>
-            <div className={`rounded items-center py-6 px-4 absolute static bg-gray-700  z-auto z-[-1] left-100 md:w-auto w-auto ml-10 pr-24 md:pl-0 pl-9  md:mr-0 transition-all duration-500 ease-in ${open ? 'top-20':'top-[-590px]'}`}>
+            <div className={`rounded items-center py-6 px-4 absolute static bg-gray-700  z-auto z-[-1] left-100 md:w-auto md:mr-[-10] w-auto ml-10 pr-24 md:pl-0 pl-9  md:mr-0 transition-all duration-500 ease-in ${open ? 'top-20':'top-[-650px]'}`}>
               <h1 className="text-3xl md:px-8 px-2 font-bold">About</h1>
-              <div className="md:grid grid-cols-4 gap-4 md:px-12 px-6 py-4">                  
+              <div className="md:grid grid-cols-5 gap-2 md:px-12 px-6 py-4">                  
                   <div>
                     <span className="text-2xl">Institute</span>
                     <ul className="px-4 py-2">
@@ -40,15 +40,24 @@ const NavBar = () => {
                   <div>
                   <span className="text-2xl">Leadership</span>
                       <ul className="px-4 py-2">
-                        <li className="pb-[3px]"><a href="" className="hover:underline">Scientist</a></li>
-                        <li className="pb-[3px]"><a href="" className="hover:underline">Academics</a></li>
-                        <li className="pb-[3px]"><a href="" className="hover:underline">Non Academics</a></li>
-                        <li className="pb-[3px]"><a href="" className="hover:underline">Volunteer</a></li>
-                        <li className="pb-[3px]"><a href="" className="hover:underline">Alumin</a></li>
+                        <li className="pb-[3px]"><a href="" className="hover:underline">Board of Govrnors</a></li>
+                        <li className="pb-[3px]"><a href="" className="hover:underline">Chairman</a></li>
+                        <li className="pb-[3px]"><a href="" className="hover:underline">Director</a></li>
+                        <li className="pb-[3px]"><a href="" className="hover:underline">Research Council</a></li>
                       </ul>
                   </div>
                   <div>
-                    <span>About</span>
+                  <span className="text-2xl">Administration</span>
+                      <ul className="px-4 py-2">
+                        <li className="pb-[3px]"><a href="" className="hover:underline">Accout Division</a></li>
+                        <li className="pb-[3px]"><a href="" className="hover:underline">Administration Division</a></li>
+                        <li className="pb-[3px]"><a href="" className="hover:underline">Computer Division</a></li>
+                        <li className="pb-[3px]"><a href="" className="hover:underline">Science Eduction and Dissemination Unit</a></li>
+                        <li className="pb-[3px]"><a href="" className="hover:underline">Instrument and Maintenance Division</a></li>
+                        <li className="pb-[3px]"><a href="" className="hover:underline">Internal Audit Division</a></li>
+                        <li className="pb-[3px]"><a href="" className="hover:underline">Office of Director</a></li>
+                        <li className="pb-[3px]"><a href="" className="hover:underline">Procurement and Laboratory Servies Division</a></li>
+                      </ul>
                   </div>
                   <div>
                     <span>About</span>
