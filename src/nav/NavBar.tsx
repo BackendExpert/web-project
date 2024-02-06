b2310f2e60017432be7b34ac9503524870e7b485
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-06 05:51:36 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/nav/NavBar.tsx
+++ src/nav/NavBar.tsx
@@ -16,15 +16,15 @@ const NavBar = () => {
             <div onClick={() => OpenMenu(!open)} className="text-xl absolute right-8 top-5 cursor-pointer">
                 <span><IonIcon name={open ? "close" : "menu"} /></span>
             </div>
-            <div className={`rounded items-center py-6 px-4 absolute static bg-gray-700  z-auto z-[-1] left-100 md:w-auto w-auto ml-10 pr-24 md:pl-0 pl-9  md:mr-0 transition-all duration-500 ease-in ${open ? 'top-20':'top-[-490px]'}`}>
+            <div className={`rounded items-center py-6 px-4 absolute static bg-gray-700  z-auto z-[-1] left-100 md:w-auto w-auto ml-10 pr-24 md:pl-0 pl-9  md:mr-0 transition-all duration-500 ease-in ${open ? 'top-20':'top-[-590px]'}`}>
               <h1 className="text-3xl md:px-8 px-2 font-bold">About</h1>
               <div className="md:grid grid-cols-4 gap-4 md:px-12 px-6 py-4">                  
                   <div>
                     <span className="text-2xl">Institute</span>
                     <ul className="px-4 py-2">
-                      <li className="pb-[3px]"><a href="">About Us</a></li>
-                      <li className="pb-[3px]"><a href="">History</a></li>
-                      <li className="pb-[3px]"><a href="">Vision and Mision</a></li>
+                      <li className="pb-[3px]"><a href="" className="hover:underline">About Us</a></li>
+                      <li className="pb-[3px]"><a href="" className="hover:underline">History</a></li>
+                      <li className="pb-[3px]"><a href="" className="hover:underline">Vision and Mision</a></li>
                     </ul>
                   </div>
                   <div>
