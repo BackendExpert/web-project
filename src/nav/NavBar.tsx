7eababf0ad3ac2b5be822ad85121f0b2907e5595
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-06 06:16:34 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/nav/NavBar.tsx
+++ src/nav/NavBar.tsx
@@ -16,9 +16,9 @@ const NavBar = () => {
             <div onClick={() => OpenMenu(!open)} className="text-xl absolute right-8 top-5 cursor-pointer">
                 <span><IonIcon name={open ? "close" : "menu"} /></span>
             </div>
-            <div className={`rounded items-center py-6 px-4 absolute static bg-gray-700  z-auto z-[-1] left-100 md:w-auto w-auto ml-10 pr-24 md:pl-0 pl-9  md:mr-10 transition-all duration-500 ease-in ${open ? 'top-20':'top-[-650px]'}`}>
+            <div className={`rounded items-center py-6 px-4 absolute static bg-gray-700  z-auto z-[-1] left-100 md:w-auto w-auto ml-0 pr-24 md:pl-0 pl-9  md:mr-10 mr-8 transition-all duration-500 ease-in ${open ? 'top-20':'top-[-2000px]'}`}>
               <h1 className="text-3xl md:px-8 px-2 font-bold">About</h1>
-              <div className="md:grid grid-cols-4 gap-1 md:px-12 px-6 py-4">                  
+              <div className="md:grid md:grid-cols-5 gap-1 md:px-12 px-0 py-4">                  
                   <div>
                     <span className="text-2xl">Institute</span>
                     <ul className="px-4 py-2">
@@ -45,9 +45,6 @@ const NavBar = () => {
                         <li className="pb-[3px]"><a href="" className="hover:underline">Director</a></li>
                         <li className="pb-[3px]"><a href="" className="hover:underline">Research Council</a></li>
                       </ul>
-                  </div>
-                  <div>
-                    <span>About</span>
                   </div>
                   <div>
                   <span className="text-2xl">Administration</span>
