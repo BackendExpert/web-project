b3fefed1b4d44e78972ae0c32009090786567263
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-06 05:46:10 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/nav/NavBar.tsx
+++ src/nav/NavBar.tsx
@@ -8,7 +8,7 @@ const NavBar = () => {
   return(
     <div className="shadow-md w-full fixed top-0 left-0 bg-blue-900">
       <div className="md:flex items-center justify-between bg-gray-700 text-white py-4 md:px-20 px-7">
-        <div className="font-bold text-2xl cursor-pointer flex items-center text-white">
+        <div className="cursor-pointer flex items-center text-white">
           <span className="text-3x1 text-indigo-600 flex">
             <img src={SlLogo} alt="" className="h-8 w-8"/>
             <span className="px-4">NIFS</span>
@@ -16,18 +16,27 @@ const NavBar = () => {
             <div onClick={() => OpenMenu(!open)} className="text-xl absolute right-8 top-5 cursor-pointer">
                 <span><IonIcon name={open ? "close" : "menu"} /></span>
             </div>
-            <div className={`rounded items-center py-6 px-4 absolute static bg-gray-700  z-auto z-[-1] left-100 md:w-auto w-auto md:pl-0 pl-9  md:mr-0 transition-all duration-500 ease-in ${open ? 'top-20':'top-[-490px]'}`}>
-              <h1 className="text-3xl md:px-8 px-2">About</h1>
+            <div className={`rounded items-center py-6 px-4 absolute static bg-gray-700  z-auto z-[-1] left-100 md:w-auto w-auto ml-10 pr-24 md:pl-0 pl-9  md:mr-0 transition-all duration-500 ease-in ${open ? 'top-20':'top-[-490px]'}`}>
+              <h1 className="text-3xl md:px-8 px-2 font-bold">About</h1>
               <div className="md:grid grid-cols-4 gap-4 md:px-12 px-6 py-4">                  
                   <div>
-                    <span>Institute</span>
-                    <ul>
-                      <li><a href=""></a></li>
+                    <span className="text-2xl">Institute</span>
+                    <ul className="px-4 py-2">
+                      <li className="pb-[3px]"><a href="">About Us</a></li>
+                      <li className="pb-[3px]"><a href="">History</a></li>
+                      <li className="pb-[3px]"><a href="">Vision and Mision</a></li>
                     </ul>
                   </div>
                   <div>
-                    <span>About</span>
-                  </div>
+                    <span className="text-2xl">People</span>
+                      <ul className="px-4 py-2">
+                        <li ><a href="">Scientist</a></li>
+                        <li className="pb-[3px]"><a href="">Academics</a></li>
+                        <li className="pb-[3px]"><a href="">Non Academics</a></li>
+                        <li className="pb-[3px]"><a href="">Volunteer</a></li>
+                        <li className="pb-[3px]"><a href="">Alumin</a></li>
+                      </ul>
+                    </div>
                   <div>
                     <span>About</span>
                   </div>
