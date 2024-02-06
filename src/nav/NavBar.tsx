84ffc76df16bd7458b4f42395c29d7fd826f329d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-06 09:48:40 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/nav/NavBar.tsx
+++ src/nav/NavBar.tsx
@@ -113,6 +113,11 @@ const NavBar = () => {
                     <span className="px-4 text-md duration-500 hover:bg-white hover:text-gray-700 rounded-sm font-medium">Vacancies</span><br />
                     <span className="px-4 text-md duration-500 hover:bg-white hover:text-gray-700 rounded-sm font-medium">Study</span><br />
                   </div>
+                  <div className="md:mt-[-320px]">
+                  <h1 className="text-3xl md:px-0 px-2 py-2 font-bold">Careers</h1> 
+                    <span className="px-4 text-md duration-500 hover:bg-white hover:text-gray-700 rounded-sm font-medium">Vacancies</span><br />
+                    <span className="px-4 text-md duration-500 hover:bg-white hover:text-gray-700 rounded-sm font-medium">Study</span><br />
+                  </div>
               </div>
             </div>
           </div>
