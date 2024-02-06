4dd32636ab30d4c2224d003d1e18590a581ad6d5
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-06 06:20:20 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/nav/NavBar.tsx
+++ src/nav/NavBar.tsx
@@ -11,14 +11,17 @@ const NavBar = () => {
         <div className="cursor-pointer flex items-center text-white">
           <span className="text-3x1 text-indigo-600 flex">
             <img src={SlLogo} alt="" className="h-8 w-8"/>
-            <span className="px-4">NIFS</span>
+            <span className="px-4 text-2xl">NIFS</span>
           </span>
-            <div onClick={() => OpenMenu(!open)} className="text-xl absolute right-8 top-5 cursor-pointer">
+          <ul className="text-xl absolute right-8 top-5 cursor-pointer pr-12"> 
+            <li>Home</li>
+          </ul>
+            <div onClick={() => OpenMenu(!open)} className="text-xl absolute right-8 top-6 cursor-pointer">
                 <span><IonIcon name={open ? "close" : "menu"} /></span>
             </div>
             <div className={`rounded items-center py-6 px-4 absolute static bg-gray-700  z-auto z-[-1] left-100 md:w-auto w-auto ml-0 pr-24 md:pl-0 pl-9  md:mr-10 mr-8 transition-all duration-500 ease-in ${open ? 'top-20':'top-[-2000px]'}`}>
               <h1 className="text-3xl md:px-8 px-2 font-bold">About</h1>
-              <div className="md:grid md:grid-cols-5 gap-1 md:px-12 px-0 py-4">                  
+              <div className="md:grid md:grid-cols-5 gap-2 md:px-12 px-0 py-4">                  
                   <div>
                     <span className="text-2xl">Institute</span>
                     <ul className="px-4 py-2">
