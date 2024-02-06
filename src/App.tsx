333bd6d9f654c1c4db24aa312b7530aaa1ba9b7d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-06 06:11:38 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import Footer from "./footer/Footer";
+import NavBar from "./nav/NavBar";
 import Tb from "./TestBody/Tb";
 
 
 export default function App() {
   return (
     <div>
-
-        <Tb />    
-        <Footer />
+      <NavBar />
+      <div>        
+          <Tb />    
+          <Footer />
+      </div>
     </div>
 
   )
--- src/nav/NavBar.tsx
+++ src/nav/NavBar.tsx
@@ -6,7 +6,7 @@ const NavBar = () => {
   let [open, OpenMenu] = useState(false);
 
   return(
-    <div className="shadow-md w-full fixed top-0 left-0 bg-blue-900">
+    <div className="shadow-md w-full top-0 left-0 bg-blue-900">
       <div className="md:flex items-center justify-between bg-gray-700 text-white py-4 md:px-20 px-7">
         <div className="cursor-pointer flex items-center text-white">
           <span className="text-3x1 text-indigo-600 flex">
@@ -16,9 +16,9 @@ const NavBar = () => {
             <div onClick={() => OpenMenu(!open)} className="text-xl absolute right-8 top-5 cursor-pointer">
                 <span><IonIcon name={open ? "close" : "menu"} /></span>
             </div>
-            <div className={`rounded items-center py-6 px-4 absolute static bg-gray-700  z-auto z-[-1] left-100 md:w-auto md:mr-[-10] w-auto ml-10 pr-24 md:pl-0 pl-9  md:mr-0 transition-all duration-500 ease-in ${open ? 'top-20':'top-[-650px]'}`}>
+            <div className={`rounded items-center py-6 px-4 absolute static bg-gray-700  z-auto z-[-1] left-100 md:w-auto w-auto ml-10 pr-24 md:pl-0 pl-9  md:mr-10 transition-all duration-500 ease-in ${open ? 'top-20':'top-[-650px]'}`}>
               <h1 className="text-3xl md:px-8 px-2 font-bold">About</h1>
-              <div className="md:grid grid-cols-5 gap-2 md:px-12 px-6 py-4">                  
+              <div className="md:grid grid-cols-4 gap-1 md:px-12 px-6 py-4">                  
                   <div>
                     <span className="text-2xl">Institute</span>
                     <ul className="px-4 py-2">
@@ -47,6 +47,9 @@ const NavBar = () => {
                       </ul>
                   </div>
                   <div>
+                    <span>About</span>
+                  </div>
+                  <div>
                   <span className="text-2xl">Administration</span>
                       <ul className="px-4 py-2">
                         <li className="pb-[3px]"><a href="" className="hover:underline">Accout Division</a></li>
@@ -58,6 +61,12 @@ const NavBar = () => {
                         <li className="pb-[3px]"><a href="" className="hover:underline">Office of Director</a></li>
                         <li className="pb-[3px]"><a href="" className="hover:underline">Procurement and Laboratory Servies Division</a></li>
                       </ul>
+                  </div>
+                  <div>
+                    <span>About</span>
+                  </div>
+                  <div>
+                    <span>About</span>
                   </div>
                   <div>
                     <span>About</span>
